a779f212a38783b76ac54e94bf64795787193778
myusername <AkhyamovMP@gmail.com>
2022-05-21 18:53:46 +0300

    init database, created the template

--- coursework/mfc/admin.py
+++ coursework/mfc/admin.py
@@ -1,3 +1,8 @@
+from ast import Pass
 from django.contrib import admin
-
+from .models import Users
+from .models import Passports
 # Register your models here.
+
+admin.site.register(Users)
+admin.site.register(Passports)
--- coursework/mfc/models.py
+++ coursework/mfc/models.py
@@ -1,3 +1,29 @@
+from pyexpat import model
 from django.db import models
+from numpy import number
+from sqlalchemy import ForeignKey, true
 
-# Create your models here.
+class Users(models.Model):
+    user_id = models.IntegerField('user_id', primary_key=true)
+    username = models.CharField('username', max_length=30)
+    password = models.CharField('password', max_length=30)
+    # passport_id = models.ForeignKey('Passport', unique=true ,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+    #def __str__(self):
+    #    return (self.user_id, self.username, self.password)
+
+
+class Passports(models.Model):
+    passport_id = models.IntegerField('passport_id', primary_key=true)
+    first_name = models.CharField('first_name', max_length=30)
+    last_name = models.CharField('last_name', max_length=30)
+    middle_name = models.CharField('middle_name', max_length=30)
+    series = models.IntegerField('series')
+    passport_number = models.IntegerField('number')
+    registration = models.TextField('registration')
+    
+    def __str__(self) -> str:
+        return super().__str__()
